352543832de938cedbea94626b707cb93c869d1b
rtavaresramos <rtavareskxd99@gmail.com>
2020-04-28 07:38:37 -0300

    finishing the mobile first layout

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,9 @@ var e = document.querySelector(".navbar-items")
 function handleToggle(){
 e.classList.toggle("toggle-off")
 }
+function handleNew(e){
+  var id = 'new-'+ e
+  
+  var info = document.getElementById(id)
+  info.classList.toggle("news-off")
+}
